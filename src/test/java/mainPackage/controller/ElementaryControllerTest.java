e5fcd41849f15abe156d8fdd2899cfb6bef5dcab
ramru <ramrush-r@mail.ru>
2022-10-24 23:25:48 +0300

    test template

---
+++ src/test/java/mainPackage/controller/ElementaryControllerTest.java
@@ -0,0 +1,23 @@
+package mainPackage.controller;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ElementaryControllerTest extends TestCase
+{
+
+    public void testHelloPage()
+    {
+    }
+
+    public void testByePage()
+    {
+    }
+
+    public void testCalculator()
+    {
+
+    }
+}
