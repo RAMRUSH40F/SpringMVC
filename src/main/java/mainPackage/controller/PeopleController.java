fea8a132f6d48ef835d4a13c8db1074dbec1d3d4
ramru <ramrush-r@mail.ru>
2022-10-26 21:12:35 +0300

    DAO и REST init

---
+++ src/main/java/mainPackage/controller/PeopleController.java
@@ -0,0 +1,22 @@
+package mainPackage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@GetMapping("/people")
+public class PeopleController
+{
+    @GetMapping()
+    public String index()
+    {
+        return null;
+    }
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id")int id, Model model){
+
+        return null;
+    }
+}
---
+++ src/main/java/mainPackage/dao/PersonDAO.java
@@ -0,0 +1,28 @@
+package mainPackage.dao;
+
+import mainPackage.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDAO
+{
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+        people.add(new Person(++PEOPLE_COUNT,"Mike"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+    }
+
+    public List<Person> index()
+    {
+        return people;
+    }
+    public  Person show(int id){
+        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    }
+
+}
