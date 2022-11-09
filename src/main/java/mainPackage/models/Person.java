c9d9887058da5167e01db7e63ceaf9d7b1cc1934
ramru <ramrush-r@mail.ru>
2022-11-09 21:22:47 +0300

    reformat

--- src/main/java/mainPackage/models/Person.java
+++ src/main/java/mainPackage/models/Person.java
@@ -1,31 +1,30 @@
 package mainPackage.models;
 
-public class Person{
-    private int id;
+public class Person {
+    private int    id;
     private String name;
 
-    public Person()
-    {
+    public Person() {
     }
 
-    public Person(int id, String name){
+    public Person(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 }
