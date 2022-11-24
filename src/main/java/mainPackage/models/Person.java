1ef1bdef1c701392aebe998e429ba4375107cc2c
ramru <ramrush-r@mail.ru>
2022-11-24 22:31:44 +0300

    validation marks modified

--- src/main/java/mainPackage/models/Person.java
+++ src/main/java/mainPackage/models/Person.java
@@ -1,8 +1,8 @@
 package mainPackage.models;
 
 
-import org.hibernate.validator.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
@@ -28,8 +28,8 @@ public class Person {
 
     public Person(int id, String name, byte age, String email) {
         this.id = id;
-        this.name = name;
         this.email = email;
+        this.name = name;
         this.age = age;
     }
 
