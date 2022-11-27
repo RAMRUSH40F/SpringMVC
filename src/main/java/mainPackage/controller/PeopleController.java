7275b481ab5df15ac7df063e76a04a5b99fc2ac4
ramru <ramrush-r@mail.ru>
2022-11-27 23:29:46 +0300

    validation marks modified

--- src/main/java/mainPackage/controller/PeopleController.java
+++ src/main/java/mainPackage/controller/PeopleController.java
@@ -8,12 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+
 
     private final PersonDAO personDAO;
 
