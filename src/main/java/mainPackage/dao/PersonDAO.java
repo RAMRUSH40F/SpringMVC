16c50a587e077d2f4fab0d7da2dec96e178390a6
ramru <ramrush-r@mail.ru>
2022-12-07 22:42:28 +0300

    validation marks modified

--- src/main/java/mainPackage/dao/PersonDAO.java
+++ src/main/java/mainPackage/dao/PersonDAO.java
@@ -7,7 +7,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 // This is a personal academic project. Dear PVS-Studio, please check it.
 
 // PVS -Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
