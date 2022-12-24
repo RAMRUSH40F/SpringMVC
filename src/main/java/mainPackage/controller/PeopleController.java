2a7a65aa44a7aa00c012928fde021dcf150d82f9
ramru <ramrush-r@mail.ru>
2022-12-24 19:04:21 +0300

    validation marks modified

--- src/main/java/mainPackage/controller/PeopleController.java
+++ src/main/java/mainPackage/controller/PeopleController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 public class PeopleController {
     private final PersonDAO personDAO;
 
+
     @Autowired
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
